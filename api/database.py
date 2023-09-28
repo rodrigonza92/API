9389a29c312f46ee46e358e8b26b1347b43e8706
Rodrigo <rodrigo.e.gonza@gmail.com>
2023-09-28 19:41:18 -0300

    Correcion espacios

--- api/database.py
+++ api/database.py
@@ -25,45 +25,46 @@ class DatabaseConnection:
         cursor = cls.get_connection().cursor()
         query = """CREATE DATABASE IF NOT EXISTS db_tif;
 
-        CREATE TABLE IF NOT EXISTS db_tif.usuario (
-            id_user INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            first_name VARCHAR (255) NOT NULL, 
-            last_name VARCHAR (255) NOT NULL,
-            birthday DATE NOT NULL,
-            email VARCHAR (255) NOT NULL,
-            username VARCHAR (255) NOT NULL,
-            passwd VARCHAR (255) NOT NULL
-        );
+CREATE TABLE IF NOT EXISTS db_tif.usuario (
+    id_user INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR (255) NOT NULL, 
+    last_name VARCHAR (255) NOT NULL,
+    birthday DATE NOT NULL,
+    email VARCHAR (255) NOT NULL,
+    username VARCHAR (255) NOT NULL,
+    passwd VARCHAR (255) NOT NULL
+);
 
-        CREATE TABLE IF NOT EXISTS db_tif.servidor (
-            id_server INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            nombre VARCHAR (255) NOT NULL,
-            descripcion VARCHAR (255) NOT NULL
-        );
+CREATE TABLE IF NOT EXISTS db_tif.servidor (
+    id_server INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR (255) NOT NULL,
+    descripcion VARCHAR (255) NOT NULL
+);
 
-        CREATE TABLE IF NOT EXISTS db_tif.canal (
-            id_canal INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            nombre VARCHAR(50) NOT NULL,
-            id_server INT NOT NULL,
-            FOREIGN KEY (id_server) REFERENCES db_tif.servidor (id_server) ON DELETE CASCADE ON UPDATE CASCADE
-        );
+CREATE TABLE IF NOT EXISTS db_tif.canal (
+    id_canal INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    id_server INT NOT NULL,
+    FOREIGN KEY (id_server) REFERENCES db_tif.servidor (id_server) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
-        CREATE TABLE IF NOT EXISTS db_tif.mensaje (
-            id_mensaje INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            id_user INT NOT NULL,
-            id_canal INT NOT NULL,
-            fecha DATETIME NOT NULL,
-            mensaje VARCHAR (255) NOT NULL,
-            FOREIGN KEY (id_user) REFERENCES db_tif.usuario (id_user) ON DELETE CASCADE ON UPDATE CASCADE,
-            FOREIGN KEY (id_canal) REFERENCES db_tif.canal (id_canal) ON DELETE CASCADE ON UPDATE CASCADE
-        );
+CREATE TABLE IF NOT EXISTS db_tif.mensaje (
+    id_mensaje INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_user INT NOT NULL,
+    id_canal INT NOT NULL,
+    fecha DATETIME NOT NULL,
+    mensaje VARCHAR (255) NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES db_tif.usuario (id_user) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_canal) REFERENCES db_tif.canal (id_canal) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
-        CREATE TABLE IF NOT EXISTS db_tif.membresia_servidor (
-            id_user INT NOT NULL,
-            id_server INT NOT NULL,
-            FOREIGN KEY (id_user) REFERENCES db_tif.usuario (id_user) ON DELETE CASCADE ON UPDATE CASCADE,
-            FOREIGN KEY (id_server) REFERENCES db_tif.servidor (id_server) ON DELETE CASCADE ON UPDATE CASCADE
-        );"""
+CREATE TABLE IF NOT EXISTS db_tif.membresia_servidor (
+    id_user INT NOT NULL,
+    id_server INT NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES db_tif.usuario (id_user) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_server) REFERENCES db_tif.servidor (id_server) ON DELETE CASCADE ON UPDATE CASCADE
+);
+        """
         cursor.execute(query)
 
     @classmethod
